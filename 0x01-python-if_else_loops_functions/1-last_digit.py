e6a19fe20ceef3dc84f64244e04cc0f70a1b804c
Realtunny <akinseyetunde2018@gmail.com>
2023-12-08 06:34:03 -0800

    complete if else loops function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit =str(number)
-last_string = int(last_digit[-1])
-if last_string > 5:
-    print(f"Last digit of {last_digit} is {last_string} and is greater than 5")
-elif last_string < 6 and last_string != 0:
-    print(f"Last digit of {last_digit} is {last_string} and is less than 6 and not 0")
-elif last_string == 0:
-    print(f"Last digit of {last_digit} is {last_string} and is zero")
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Akinseye Tunde
+
+def pow(a, b):
+    return (a ** b)
